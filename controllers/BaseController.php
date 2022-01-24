8360a2cd79d15a5b3d211d29deaf283c70b6d64f
cuongnmparaline <97074186+cuongnmparaline@users.noreply.github.com>
2022-01-24 13:39:59 +0700

    Morning 24-1-2022

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -8,19 +8,14 @@ class BaseController
 
     function render($file, $data = array())
     {
-
         $view_file = 'views/' . $this->folder . '/' . $file . '.php';
         if (is_file($view_file)) {
-
             extract($data);
-            // Sau đó lưu giá trị trả về khi chạy file view template với các dữ liệu đó vào 1 biến chứ chưa hiển thị luôn ra trình duyệt
             ob_start();
             require_once($view_file);
             $content = ob_get_clean();
-            // Sau khi có kết quả đã được lưu vào biến $content, gọi ra template chung của hệ thống đế hiển thị ra cho người dùng
             require_once('views/layouts/application.php');
         } else {
-            // Nếu file muốn gọi ra không tồn tại thì chuyển hướng đến trang báo lỗi.
             header('Location: search.php?controller=pages&action=error');
         }
     }
